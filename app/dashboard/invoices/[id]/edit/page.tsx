9c392ce7d7a7a0b20a6bf84144d15362e89f8b92
cmd-anurag <anuragkumar.7157@gmail.com>
2025-03-23 23:18:44 +0530

    chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation';
 export default async function Page(props: {params: Promise<{id: string}>}) {
     const params = await props.params;
     const id = params.id;
@@ -11,7 +11,9 @@ export default async function Page(props: {params: Promise<{id: string}>}) {
         fetchCustomers(),
     ]);
 
-    
+    if(!invoice) {
+      notFound();
+    }
 
 
   return (
